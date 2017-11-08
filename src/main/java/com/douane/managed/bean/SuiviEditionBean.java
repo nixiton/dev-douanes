bf24144c951f1dc46accd48b9f7817f1ac4f203b
nixiton <nyaina.andriambolamalala@gmail.com>
2017-11-08 22:06:47 +0100

    nix DGD 2

--- src/main/java/com/douane/managed/bean/SuiviEditionBean.java
+++ src/main/java/com/douane/managed/bean/SuiviEditionBean.java
@@ -16,9 +16,6 @@ public class SuiviEditionBean {
     @Autowired
     IUserMetier usermetierimpl;
 
-
-
-
     private Agent agentOperateur;
     private Direction direction;
     private Date startDate;
