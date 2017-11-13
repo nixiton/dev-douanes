1324d8ded1813569732302d67b720dbe2ccf7c73
nixiton <nyaina.andriambolamalala@gmail.com>
2017-11-13 23:00:48 +0100

    nix attrib 20

--- src/main/java/com/douane/managed/bean/DepositaireBean.java
+++ src/main/java/com/douane/managed/bean/DepositaireBean.java
@@ -1358,7 +1358,7 @@ System.out.println("****************************ADD3 ATTR**ERRORR***************
 		this.currentListMateriel = currentListMateriel;
 	}
 	public void mySetCurrentListMateriel(){
-		
+		System.out.println("****************************SET LIST ******************************** " +this.getDetenteur().im);
 		this.setCurrentListMateriel(usermetierimpl.getMatByDetenteurAndValidation(this.getDetenteur(), true));
 	}
 }
