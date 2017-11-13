1f0f8825ed6e33371f1958c13667b8161e138c14
tex31 <nyhoavy_nomena@yahoo.ca>
2017-11-13 10:45:35 +0300

    add functions to operationDAO

--- src/main/java/come/douane/dao/operation/IOperationDAO.java
+++ src/main/java/come/douane/dao/operation/IOperationDAO.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.douane.entite.Agent;
+import com.douane.entite.Direction;
 import com.douane.entite.Materiel;
 import com.douane.entite.OpAttribution;
 import com.douane.entite.OpDettachement;
@@ -14,8 +15,31 @@ import com.douane.entite.Operation;
 public interface IOperationDAO {
 	public Agent detacherMat(OpDettachement det);
 	public Materiel attribuerMat(OpAttribution attr);
-	public List<Operation> getListOpByDate(Date startDate, Date endDate );
 	
-	public List<OpEntree> getListOpEntreeByByMaterielBDate(Materiel m, Date startDate, Date endDate);
-	public List<OpSortie> getListOpSortieByByMaterielBDate(Materiel m, Date startDate, Date endDate);
+	public List<Operation> getListOpByDate(Date startDate, Date endDate , int maxresult);
+	
+	
+	public List<OpEntree> getListOpEntreeOrderByDate(int maxresult);
+	public List<OpSortie> getListOpSortieOrderByDate(int maxresult);
+	public List<Operation> getListOperationOrderByDate(int maxresult);
+	public List<OpAttribution> getListOpAttributionOrderByDate(int maxresult);
+	public List<OpDettachement> getListOpDettachementOrderByDate(int maxresult);
+	
+	public List<OpEntree> getListOpEntreeByDirOrder(Direction dir, int maxresult);
+	public List<OpSortie> getListOpSortieByDirOrder(Direction dir, int maxresult);
+	public List<Operation> getListOperationByDirOrder(Direction dir, int maxresult);
+	public List<OpAttribution> getListOpAttributionByDirOrder(Direction dir, int maxresult);
+	public List<OpDettachement> getListOpDettachementByDirOrder(Direction dir, int maxresult);
+	
+	public List<OpEntree> getListOpEntreeByOperator(Agent operator, int maxresult);
+	public List<OpSortie> getListOpSortieByOperator(Agent operator, int maxresult);
+	public List<Operation> getListOperationByOperator(Agent operator, int maxresult);
+	public List<OpAttribution> getListOpAttributionByOperator(Agent operator, int maxresult);
+	public List<OpDettachement> getListOpDettachementByOperator(Agent operator, int maxresult);
+	
+	public List<OpEntree> getListOpEntreeByByMaterielBDate(Materiel m, Date startDate, Date endDate, int maxresult);
+	public List<OpSortie> getListOpSortieByByMaterielBDate(Materiel m, Date startDate, Date endDate, int maxresult);
+	
+	
+
 }
