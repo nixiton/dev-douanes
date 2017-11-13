a45e16567919134903e00619c8f80aed3e276343
nixiton <nyaina.andriambolamalala@gmail.com>
2017-11-13 08:33:35 +0100

    nix operation 60

--- src/main/java/com/douane/managed/bean/SuiviEditionBean.java
+++ src/main/java/com/douane/managed/bean/SuiviEditionBean.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import java.util.Date;
 import java.util.List;
 import org.springframework.stereotype.Component;
@@ -17,7 +18,7 @@ import org.springframework.stereotype.Component;
 
 
 @ManagedBean(name="suivieditionBean")
-@RequestScoped
+@ViewScoped
 public class SuiviEditionBean {
 
 
